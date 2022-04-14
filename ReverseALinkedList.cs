c3b8d9b9bb8f42b44c4f341a031e0d27ea722a9f
uygarbilge <uygarbilge@msn.com>
2022-04-14 12:21:28 +0300

    solved

---
+++ ReverseALinkedList.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public  class ReverseALinkedList
+    {
+
+        public static SinglyLinkedListNode reverse(SinglyLinkedListNode llist)
+        { 
+            Stack<int> stack = new Stack<int>(); 
+            SinglyLinkedListNode n = llist;
+
+
+            while (n != null)
+            {
+                stack.Push(n.data);
+                n = n.next;
+            }
+            
+            SinglyLinkedList newList = new SinglyLinkedList();
+      
+
+            while (stack.Count > 0)
+            {
+                newList.InsertNode(stack.Pop());
+            }
+
+
+            return newList.head;
+        }
+
+
+        public static void PrintSinglyLinkedList(SinglyLinkedListNode node, string sep)
+        {
+            while (node != null)
+            {
+                Console.WriteLine(node.data);
+
+                node = node.next;
+
+                if (node != null)
+                {
+                    Console.Write(sep);
+                }
+            }
+        }
+
+        public void Run()
+        {
+
+            int tests = Convert.ToInt32(Console.ReadLine());
+
+            for (int testsItr = 0; testsItr < tests; testsItr++)
+            {
+                SinglyLinkedList llist = new SinglyLinkedList();
+
+                int llistCount = Convert.ToInt32(Console.ReadLine());
+
+                for (int i = 0; i < llistCount; i++)
+                {
+                    int llistItem = Convert.ToInt32(Console.ReadLine());
+                    llist.InsertNode(llistItem);
+                }
+
+            
+                SinglyLinkedListNode llist1 = reverse(llist.head);
+                PrintSinglyLinkedList(llist1, " ");
+
+
+            }
+
+        }
+
+    }
+
+   public  class SinglyLinkedListNode
+    {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData)
+        {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    public class SinglyLinkedList
+    {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList()
+        {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void InsertNode(int nodeData)
+        {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+
+
+
+
+}
+
