8f9e9ae9cc895c74ba099ec47a0ed3eb70c9d696
uygarbilge <uygarbilge@msn.com>
2022-04-16 22:20:43 +0300

    solved

---
+++ IceCreamParlor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class IceCreamParlor
+    {
+
+        public static List<int> icecreamParlor(int m, List<int> arr)
+        {
+            List<int> list = new List<int>();
+
+            for(int i = 0; i < arr.Count - 1; i++)
+                for(int j = i + 1; j < arr.Count; j++)
+                {
+                    if(arr[i] + arr[j] == m)
+                    {
+                        list.Add(i+1);
+                        list.Add(j +1);
+                        break;
+                    }
+                }
+
+            return list;
+        }
+
+
+        public void Run()
+        {
+            int m = 6;
+
+            List<int> arr = new List<int>() { 1, 3 , 4 , 5 , 6 };
+
+
+            
+           var result =  icecreamParlor(m, arr);
+           
+            foreach(int i in result)    
+                Console.WriteLine(i);
+
+        }
+    }
+}
---
+++ QueueUsingTwoStacks.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class QueueUsingTwoStacks
+
+    {
+
+      
+        public void Run()
+        {
+
+            Queue<int> queue = new Queue<int>();
+
+            int q = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < q; i++)
+            {
+                List<int> values = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+                int command = values[0];
+
+
+                if (command == 1) //Enqueue
+                {
+
+                    int value = values[1];
+
+                    queue.Enqueue(value);
+
+                }
+                else if(command == 2)  //dequeue
+                {
+                    queue.Dequeue();
+                    
+                }
+                else if(command == 3)  //print the front element
+                {
+                    Console.WriteLine(queue.Peek());
+                }
+
+
+            }
+        }
+
+        //public static void ProcessCommand( int commandType, int value)
+        //{
+        //    if(commandType == 1)
+        //    {
+
+        //    }
+        //    else if(commandType == 2)
+        //    {
+
+        //    }
+        //    else if(commandType == 3)
+        //    {
+
+        //    }
+        //}
+    
+    
+    }
+}
